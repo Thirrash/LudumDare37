56b123b81b49a6438b11af7298fb3200f6800ab7
Qurtaz <piwowino1994@gamil.com>
2016-12-10 09:58:29 +0100

    add scripts and flares

---
+++ Assets/Scripts/Enviroment/Sun.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class Light
+{
+    public GameObject west;
+    public GameObject east;
+    public GameObject north;
+    public GameObject south;
+}
+
+public class Sun : MonoBehaviour {
+
+    public GameObject target;
+
+    public Light down;
+    public Light up;
+
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Time.deltaTime, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Globals
+{
+    #region Static Constants
+    public static class PlayerInput
+    {
+        public static string Horizontal   = "Horizontal";
+        public static string Vertical     = "Vertical";
+        public static string Jump         = "Jump";
+        public static string MouseX       = "Mouse X";
+        public static string MouseY       = "Mouse Y";
+        public static string MouseScroll  = "Mouse ScrollWheel";
+        public static string Fire1        = "Fire1";
+        public static string Fire2        = "Fire2";
+        public static string Fire3        = "Fire3";
+        public static string Run          = "Run";
+        public static string Cancel       = "Cancel";
+        public static string Submit       = "Submit";
+    }
+
+    public static class AnimatorCondiotion
+    {
+        public static string Speed          ="Speed";
+        public static string Direction      = "Direction";
+        public static string Grounded       = "Grounded";
+        public static string AirVelocity    = "Air Velocity";
+    }
+
+    public static class AnimatorLayer
+    {
+        public static int BaseLayer = 0;
+        public static int UpperBody = 1;
+        public static int LowerBody = 2;
+    }
+    #endregion
+}
