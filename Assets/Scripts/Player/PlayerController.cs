60a26797534168a63a16389c8376ad945f2a6dc4
Thirrash <thirrasch@gmail.com>
2016-12-10 13:36:09 +0100

    Player picking objects foundation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
     PlayerStatistics stats;
+    public float maxWeightChangePerMeterAndKilo = 0.0f;
 
 	void Start () {
         stats = GetComponent<PlayerStatistics>( );
@@ -11,6 +12,7 @@ public class PlayerController : MonoBehaviour
         EventManager.StartListening( EventTypes.playerBackwards, MoveBackwards );
         EventManager.StartListening( EventTypes.playerLeft, MoveLeft );
         EventManager.StartListening( EventTypes.playerRight, MoveRight);
+        EventManager.StartListening( EventTypes.playerPick, Pick );
 	}
 
     void OnDestroy( ) {
@@ -22,17 +24,25 @@ public class PlayerController : MonoBehaviour
 	
     void MoveForward( ) {
         transform.Translate( transform.forward * stats.movementVerticalSpeed, Space.World );
+        stats.maxWeight += maxWeightChangePerMeterAndKilo * stats.movementVerticalSpeed * stats.currWeight;
     }
 
     void MoveBackwards( ) {
         transform.Translate( -transform.forward * stats.movementVerticalSpeed, Space.World );
+        stats.maxWeight += maxWeightChangePerMeterAndKilo * stats.movementVerticalSpeed * stats.currWeight;
     }
 
     void MoveLeft( ) {
         transform.Translate( -transform.right * stats.movementHorizontalSpeed, Space.World );
+        stats.maxWeight += maxWeightChangePerMeterAndKilo * stats.movementHorizontalSpeed * stats.currWeight;
     }
 
     void MoveRight( ) {
         transform.Translate( transform.right * stats.movementHorizontalSpeed, Space.World );
+        stats.maxWeight += maxWeightChangePerMeterAndKilo * stats.movementHorizontalSpeed * stats.currWeight;
+    }
+
+    void Pick( ) {
+
     }
 }
