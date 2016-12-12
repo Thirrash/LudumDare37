e457d03836aba23499015ec509a06ad4e0047c63
Qurtaz <piwowino1994@gamil.com>
2016-12-12 15:47:03 +0100

    add script

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+    public GameObject plane;
+    public Text text;
+	public void Exit()
+    {
+        Application.Quit();
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Credicts()
+    {
+        plane.SetActive(true);
+        text.text = "Game created: \nPaweł Winiecki \nMariusz Sielicki\n Textures taken from:\n www.flaticon.com \nFrom username: \nFreepik\nMadebyoliver\nEucalyp\nIconnice";
+    }
+    public void Close()
+    {
+        plane.SetActive(false);
+    }
+}
